29a468d486cb399e932479dba70fecc45759fe90
Leonardo Fernandes <leonardo.fernandes.proway@gmail.com>
2023-10-06 21:10:51 -0300

    finalizado atividade 4

---
+++ atividade4/android/app/src/main/kotlin/com/example/atividade4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ atividade4/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meu App 3'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              _getItem("Linha 1"),
+            ],
+          ),
+          Row(
+            children: [
+              _getItem("Lado 1", 1),
+              _getItem("Lado 2", 2),
+            ],
+          ),
+          Row(
+            children: [
+              _getItem("Linha 3"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _getItem(String label, [int flexValue = 1]) {
+    return Expanded(
+      flex: flexValue,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        color: Colors.grey,
+        height: 50,
+        child: Center(
+          child: Text(label, style: TextStyle(color: Colors.white, fontSize: 20),),
+        ),
+      ),
+    );
+  }
+}
