2aacdcbb3c7dfa11c69e98c1dc67fdd5323bca19
Leonardo Fernandes <leonardo.fernandes.proway@gmail.com>
2023-10-06 21:12:00 -0300

    atualizado atividade 4

--- atividade4/lib/main.dart
+++ atividade4/lib/main.dart
@@ -13,7 +13,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meu App 3'),
+        title: Text('Meu App 4'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
