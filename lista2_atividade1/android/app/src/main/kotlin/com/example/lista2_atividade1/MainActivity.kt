75026e65e9707a179382f4a4a2536b0f47695c9a
Leonardo Fernandes <leonardo.fernandes.proway@gmail.com>
2023-10-10 21:23:14 -0300

    Adicionado lista2

---
+++ lista2_atividade1/android/app/src/main/kotlin/com/example/lista2_atividade1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lista2_atividade1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lista2_atividade1/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _contador = 0;
+
+  _incrementar(){
+    setState(() {
+      _contador++;
+    });
+  }
+
+  _decrementar(){
+    setState(() {
+      _contador--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Contador Pessoas',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image/fundo.jpg'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$_contador',
+              style: TextStyle(fontSize: 80, color: (_contador == 10 ? Colors.red : Colors.white)),
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(100, 100),
+                    backgroundColor: Colors.amber,
+                  ),
+                  onPressed: (_contador > 0 ? _decrementar : null),
+                  child: const Text(
+                    '-',
+                    style: TextStyle(fontSize: 40, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(100, 100),
+                    backgroundColor: Colors.amber,
+                  ),
+                  onPressed: (_contador < 10 ? _incrementar : null),
+                  child: const Text(
+                    '+',
+                    style: TextStyle(fontSize: 40, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
