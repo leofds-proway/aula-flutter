43c3ab4b5f2030f683322203d733b7ddd3c140f6
Leonardo Fernandes <leonardo.fernandes.proway@gmail.com>
2023-10-10 21:45:22 -0300

    Atualizado lista2

--- lista2_atividade1/lib/main.dart
+++ lista2_atividade1/lib/main.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text(
           'Contador Pessoas',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(color: Colors.black, fontFamily: 'SuperDessert'),
         ),
         backgroundColor: Colors.amber,
       ),
