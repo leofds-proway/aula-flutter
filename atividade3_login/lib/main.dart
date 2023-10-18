c9b08aea50911ebcd379a7a707a44bb2a777922a
Leonardo Fernandes <leonardo.fernandes@PROWAY.TREINA>
2023-10-18 20:17:09 -0300

    Atualizado atividade 3

--- atividade3_login/lib/main.dart
+++ atividade3_login/lib/main.dart
@@ -6,8 +6,83 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+
+  String? _erroEmail;
+  String? _erroSenha;
+
+  _sucessoLogin() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Login'),
+        content: Text('Sucesso'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Ok')),
+        ],
+      ),
+    );
+  }
+
+  _falhaLogin() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Login'),
+        content: Text('Inválido'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Ok')),
+        ],
+      ),
+    );
+  }
+
+  bool _validarCampos(String email, String senha) {
+    _erroEmail = null;
+    _erroSenha = null;
+    if (email.isEmpty) {
+      _erroEmail = "E-mail é obrigatório";
+    }
+    if (senha.isEmpty) {
+      _erroSenha = "Senha é obrigatório";
+    }
+    return _erroEmail == null && _erroSenha == null;
+  }
+
+  bool _autenticar(String email, String senha) {
+    return email == "leo" && senha == "1234";
+  }
+
+  _click() {
+    String email = _emailController.text;
+    String senha = _senhaController.text;
+
+    if (_validarCampos(email, senha)) {
+      if (_autenticar(email, senha)) {
+        _sucessoLogin();
+      } else {
+        _falhaLogin();
+      }
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,48 +91,62 @@ class Home extends StatelessWidget {
         title: Text('Login'),
         backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/image/dog.png',
-              height: 150,
-            ),
-            const Row(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Pet House',
-                  style: TextStyle(fontSize: 40),
+                Image.asset(
+                  'assets/image/dog.png',
+                  height: 120,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Pet House',
+                      style: TextStyle(fontSize: 40),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 15),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "E-mail",
+                      hintText: "Informe seu e-mail",
+                      errorText: _erroEmail),
+                ),
+                const SizedBox(height: 15),
+                TextField(
+                  controller: _senhaController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Senha",
+                      hintText: "Informe sua senha",
+                      errorText: _erroSenha),
+                  obscureText: true,
+                  //dobscuringCharacter: "*",
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: _click,
+                        child: Text('Login'),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ),
-            const SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 15),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 15),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
-                  ),
-                ),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
