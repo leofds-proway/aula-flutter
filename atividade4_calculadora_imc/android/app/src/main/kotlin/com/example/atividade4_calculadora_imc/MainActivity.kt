fa8a2b7889f784d4217e4bed9492936a294d5b9e
Leonardo Fernandes <leonardo.fernandes@PROWAY.TREINA>
2023-10-19 20:16:44 -0300

    atividade4

---
+++ atividade4_calculadora_imc/android/app/src/main/kotlin/com/example/atividade4_calculadora_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade4_calculadora_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ atividade4_calculadora_imc/lib/main.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(home: Home(),));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora IMC'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Informe')
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('IMC')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(child: TextField()),
+                    const SizedBox(width: 20,),
+                    Expanded(child: TextField()),
+                  ],
+                ),
+                const SizedBox(height: 10,),
+                Row(
+                  children: [
+                    Expanded(child: ElevatedButton(
+                      onPressed: _clickCalcular,
+                      child: const Text('Calcular'),
+                    ))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _clickCalcular(){
+
+  }
+}
